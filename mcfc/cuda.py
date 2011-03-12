35ea61b65c67882782ee409475b46f79b579a2c8
gmarkall <grm08@doc.ic.ac.uk>
2011-03-12 16:31:33 +0000

    Added partitioner that breaks up expressions into ones that fit into individual loop nests.

--- mcfc/cuda.py
+++ mcfc/cuda.py
@@ -44,55 +44,61 @@ def buildParameterList(tree):
     paramList = ParameterList(params)
     return paramList
 
-class LoopNestBuilder(Transformer):
+def buildLoopNest(form):
+    form_data = form.form_data()
+    rank = form_data.rank
+    integrand = form.integrals()[0].integrand()
 
-    def build(self, form):
-        form_data = form.form_data()
-	rank = form_data.rank
-	integrand = form.integrals()[0].integrand()
+    # The element loop is the outermost loop
+    loop = buildElementLoop()
+    outerLoop = loop
 
-        # The element loop is the outermost loop
-	loop = buildElementLoop()
-	outerLoop = loop
+    # Build the loop over the first rank, which always exists
+    indVarName = rankInductionVariable(0)
+    rankLoop = buildSimpleForLoop(indVarName, numNodesPerEle)
+    loop.append(rankLoop)
+    loop = rankLoop
 
-	# Build the loop over the first rank, which always exists
-	indVarName = rankInductionVariable(0)
+    # Add another loop for each rank of the form (probably no
+    # more than one more... )
+    for r in range(1,rank):
+	indVarName = rankInductionVariable(r)
 	rankLoop = buildSimpleForLoop(indVarName, numNodesPerEle)
-        loop.append(rankLoop)
+	loop.append(rankLoop)
 	loop = rankLoop
+    
+    # Add a loop for the quadrature
+    indVarName = gaussInductionVariable()
+    gaussLoop = buildSimpleForLoop(indVarName, numGaussPoints)
+    loop.append(gaussLoop)
+    loop = gaussLoop
 
-	# Add another loop for each rank of the form (probably no
-	# more than one more... )
-	for r in range(1,rank):
-	    indVarName = rankInductionVariable(r)
-	    rankLoop = buildSimpleForLoop(indVarName, numNodesPerEle)
-	    loop.append(rankLoop)
-	    loop = rankLoop
-	
-        # Add a loop for the quadrature
-	indVarName = gaussInductionVariable()
-	gaussLoop = buildSimpleForLoop(indVarName, numGaussPoints)
-	loop.append(gaussLoop)
-	loop = gaussLoop
-
-	# Determine how many dimension loops we need by inspection
-	self._indexSumDepth = 0
-	self._maxIndexSumDepth = 0
-	self.visit(integrand)
-
-	# Add loops for each dimension as necessary. 
-        for d in range(self._maxIndexSumDepth):
-	    indVarName = dimInductionVariable(d)
-	    dimLoop = buildSimpleForLoop(indVarName, numDimensions)
-	    loop.append(dimLoop)
-	    loop = dimLoop
-
-	# Hand back the outer loop, so it can be inserted into some
-	# scope.
-	return outerLoop
-
+    # Determine how many dimension loops we need by inspection.
     # We count the nesting depth of IndexSums to determine
     # how many dimension loops we need.
+    ISC = IndexSumCounter()
+    numDimLoops = ISC.count(integrand)
+
+    # Add loops for each dimension as necessary. 
+    for d in range(numDimLoops):
+	indVarName = dimInductionVariable(d)
+	dimLoop = buildSimpleForLoop(indVarName, numDimensions)
+	loop.append(dimLoop)
+	loop = dimLoop
+
+    # Hand back the outer loop, so it can be inserted into some
+    # scope.
+    return outerLoop
+
+class IndexSumCounter(Transformer):
+    "Count how many IndexSums are nested inside a tree."
+
+    def count(self, tree):
+        self._indexSumDepth = 0
+        self._maxIndexSumDepth = 0
+        self.visit(tree)
+	return self._maxIndexSumDepth
+
     def index_sum(self, tree):
 	
 	summand, indices = tree.operands()
@@ -105,12 +111,46 @@ class LoopNestBuilder(Transformer):
 
 	self._indexSumDepth = self._indexSumDepth - 1
 
-    # We don't care about any other node
+    # We don't care about any other node.
     def expr(self, tree, *ops):
         pass
 
     def terminal(self, tree):
         pass
+
+class Partitioner(Transformer):
+    """Partitions the expression up so that each partition fits inside
+    strictly on loop in the local assembly loop nest.
+    Returns a list of the partitions, and their depth (starting from
+    inside the quadrature loop)."""
+
+    def partition(self, tree):
+        self._partitions = []
+	self._ISC = IndexSumCounter()
+	self.visit(tree)
+	return self._partitions
+
+    def sum(self, tree):
+        ops = tree.operands()
+	lDepth = self._ISC.count(ops[0])
+	rDepth = self._ISC.count(ops[1])
+	# If both sides have the same nesting level:
+	if lDepth == rDepth:
+	    self._partitions.append((tree,lDepth))
+	    return
+	else:
+	    self.visit(ops[0])
+	    self.visit(ops[1])
+    
+    # If it's not a sum, then there shouldn't be any partitioning
+    # of the tree anyway.
+    def expr(self, tree):
+        depth = self._ISC.count(tree)
+        self._partitions.append((tree,depth))
+
+def findPartitions(tree):
+    part = Partitioner()
+    return part.partition(tree)
 
 def buildElementLoop():
     indVarName = eleInductionVariable()
@@ -135,10 +175,6 @@ def dimInductionVariable(count):
     name = "i_d_%d" % (count)
     return name
 
-def buildLoopNest(form):
-    LNB = LoopNestBuilder()
-    return LNB.build(form)
-
 def buildKernel(form):
 
     if form.form_data() is None:
@@ -146,20 +182,31 @@ def buildKernel(form):
 
     integrand = form.integrals()[0].integrand()
     rank = form.form_data().rank
+    
+    # Things for kernel declaration.
     t = Void()
     name = "kernel" # Fix later
     params = buildParameterList(integrand)
-    #expression = buildExpression(integrand)
-    loop = buildLoopNest(form)
+    
+    # Build the loop nest
+    loopNest = buildLoopNest(form)
 
     # Initialise the local tensor values to 0
     initialiser = buildLocalTensorInitialiser(form)
     depth = rank + 1 # Rank + element loop
-    innerLoopBody = getScopeFromNest(loop, depth)
-    innerLoopBody.prepend(initialiser)
+    loopBody = getScopeFromNest(loopNest, depth)
+    loopBody.prepend(initialiser)
+
+    # Insert the expressions into the loop nest
+    partitions = findPartitions(integrand)
+    for (tree, depth) in partitions:
+        expression = buildExpression(tree)
+	exprDepth = depth + rank + 2 # 2 = Ele loop + gauss loop
+	loopBody = getScopeFromNest(loopNest, exprDepth)
+	loopBody.prepend(expression)
 
     # Build the function with the loop nest inside
-    statements = [loop]
+    statements = [loopNest]
     body = Scope(statements)
     kernel = FunctionDefinition(t, name, params, body)
     return kernel
@@ -173,8 +220,15 @@ def getScopeFromNest(nest, depth):
     return body
 
 def buildLocalTensorInitialiser(form):
+    lhs = buildLocalTensorAccessor(form)
+    rhs = Literal(0.0)
+    initialiser = AssignmentOp(lhs, rhs)
+    return initialiser
+
+def buildLocalTensorAccessor(form):
     form_data = form.form_data()
     rank = form_data.rank
+    
     # First index is the element index
     indices = [ElementIndex()]
 
@@ -183,11 +237,10 @@ def buildLocalTensorInitialiser(form):
         indices.append(RankIndex(r))
     offset = buildOffset(indices)
     
-    # Initialise this element to 0.0
-    lhs = Subscript(localTensor, offset)
-    rhs = Literal(0.0)
-    initialiser = AssignmentOp(lhs, rhs)
-    return initialiser
+    # Subscript the local tensor variable
+    expr = Subscript(localTensor, offset)
+    return expr
+    
 
 def buildOffset(indices):
     # Start our expression with the first index
