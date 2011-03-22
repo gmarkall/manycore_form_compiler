06926324e32cafb08869dfbcd1d6d053cf9a4ed6
gmarkall <grm08@doc.ic.ac.uk>
2011-03-22 10:40:57 +0000

    update testsetup so it can be used to drive the assembler

--- mcfc/frontend.py
+++ mcfc/frontend.py
@@ -1,37 +1,73 @@
-# The usual suspects
+"""    Frontend. Usage:
+ 
+    frontend.py [options] input
+
+    Options can be: 
+    
+      --visualise, -v (to output a visualisation of the AST)
+      -o:<filename>   (to specify the output filename)"""
+
+# Python libs
 import sys, getopt
 # ANTLR runtime and generated code
 import antlr3
 from uflLexer import uflLexer
 from uflParser import uflParser
-# Visualiser, handy for debugging
+# MCFC libs
 import visualiser
+import canonicaliser
 
 def main():
 
-    try: 
-        opts, args = getopt.getopt(sys.argv[1:], "ho:", ["help"])
-    except getopt.error, msg:
-        print msg
-	print __doc__
-	sys.exit(-1)
+    opts,args = get_options()
+    keys = opts.keys
 
     if len(args) > 0: 
         inputFile = args[0]
     else:
         print "No input."
+	print __doc__
 	sys.exit(-1)
 
     root = readSource(inputFile)
 
-    outputFile = inputFile[:-4] + "pdf"
-    v = visualiser.Visualiser(outputFile)
-    v.visualise(root)
+    if 'visualise' in keys or 'v' in keys:
+        if 'o' in keys:
+	    outputFile = opts['o']
+	else:
+	    outputFile = inputFile[:-4] + "pdf"
+        visualise(root, outputFile)
+	return 0
+	
 
     return 0
 
+def get_options():
+    try: 
+        opts, args = getopt.getopt(sys.argv[1:], "hvo:", ["visualise"])
+    except getopt.error, msg:
+        print msg
+	print __doc__
+	sys.exit(-1)
+
+    opts_dict = {}
+    for opt in opts:
+        key = opt[0].lstrip('-')
+	value = opt[1]
+	opts_dict[key] = value
+    
+    return opts_dict, args
+
+def visualise(ast, filename):
+
+    v = visualiser.Visualiser(filename)
+    v.visualise(root)
+
 def readSource(inputFile):
-    charStream = antlr3.ANTLRFileStream(inputFile)
+
+    canned = canonicaliser.canonicalise(inputFile)
+    print canned
+    charStream = antlr3.ANTLRStringStream(canned)
     lexer = uflLexer(charStream)
     tokens = antlr3.CommonTokenStream(lexer)
     tokens.discardOffChannelTokens = True
