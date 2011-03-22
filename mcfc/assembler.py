a0acd5153902de25bc2588f2582e94405866501d
gmarkall <grm08@doc.ic.ac.uk>
2011-03-22 10:41:53 +0000

    Break up cudaassembler into itself and assembler, which will contain generic routines

---
+++ mcfc/assembler.py
@@ -0,0 +1,57 @@
+from visitor import *
+
+class AccessedFieldFinder(AntlrVisitor):
+
+    def __init__(self):
+        AntlrVisitor.__init__(self, preOrder)
+
+    def find(self, tree):
+        self._fields = []
+	self.traverse(tree)
+	return self._fields
+
+    def visit(self, tree):
+        label = str(tree)
+
+	if label == '=':
+	    rhs = tree.getChild(1)
+	    if str(rhs) in ['scalar_fields', 'vector_fields', 'tensor_fields']:
+		field = str(rhs.getChild(0))
+		# Strip off the quotes
+		field = field[1:-1]
+		self._fields.append(field)
+
+    def pop(self):
+        pass
+
+def findAccessedFields(tree):
+    AFF = AccessedFieldFinder()
+    return AFF.find(tree)
+
+class SolveResultFinder(AntlrVisitor):
+
+    def __init__(self):
+        AntlrVisitor.__init__(self, preOrder)
+
+    def find(self, tree):
+        self._results = []
+	self.traverse(tree)
+	return self._results
+
+    def visit(self, tree):
+        label = str(tree)
+
+	if label == '=':
+	    rhs = tree.getChild(1)
+	    if str(rhs) == 'solve':
+	        result = str(tree.getChild(0))
+		self._results.append(result)
+
+    def pop(self):
+        pass
+
+def findSolveResults(tree):
+    SRF = SolveResultFinder()
+    return SRF.find(tree)
+
+
