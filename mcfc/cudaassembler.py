b5b10551d3d1ec9aaf3876f45d7ec4fcbc037173
gmarkall <grm08@doc.ic.ac.uk>
2011-03-25 07:05:12 +0000

    move CoefficientFieldFinder to assembler.py, since it should be pretty generic

--- mcfc/cudaassembler.py
+++ mcfc/cudaassembler.py
@@ -445,36 +445,4 @@ def generateKernelParameters(tree, form):
     KPG = KernelParameterGenerator()
     return KPG.generate(tree, form)
 
-class CoefficientFieldFinder(AntlrVisitor):
-    """Given a coefficient, this class traverses the
-    AST and finds the name of the field it came from"""
 
-    def __init__(self):
-        AntlrVisitor.__init__(self, preOrder)
-
-    def find(self, ast, coeff):
-        self._count = str(coeff.count())
-	self._field = None
-	self.traverse(ast)
-	return self._field
-
-    def visit(self, tree):
-        label = str(tree)
-
-	if label == 'Coefficient':
-	    count = str(tree.getChild(1))
-	    if count == self._count:
-	        # Found the correct Field. However, we need to make sure this
-		# was a version of the field alone on the right-hand side of 
-		# an assignment.
-		field = tree.getParent()
-		fieldParent = field.getParent()
-		if str(fieldParent) == '=':
-		    self._field = str(field)
-
-    def pop(self):
-        pass
-
-def findFieldFromCoefficient(ast, coeff):
-    CFF = CoefficientFieldFinder()
-    return CFF.find(ast, coeff)
