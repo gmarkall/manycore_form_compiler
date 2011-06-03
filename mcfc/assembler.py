acd2f4b1adff887ef7527152f81c98c602c781d4
gmarkall <grm08@doc.ic.ac.uk>
2011-06-03 11:15:19 +0100

    Change the accessed field finder to use the new AST

--- mcfc/assembler.py
+++ mcfc/assembler.py
@@ -18,6 +18,7 @@
 # holders.
 
 from visitor import *
+from ast import NodeVisitor
 
 # Placeholder; will probably fill with things later on.
 # Should be inherited by all assembler implementations.
@@ -26,29 +27,56 @@ class AssemblerBackend:
     def compile(self, ast, uflObjects):
         raise NotImplementedError("You're supposed to implement compile()!")
 
-class AccessedFieldFinder(AntlrVisitor):
-
-    def __init__(self):
-        AntlrVisitor.__init__(self, preOrder)
+class AccessedFieldFinder(NodeVisitor):
 
     def find(self, tree):
         self._fields = []
-        self.traverse(tree)
-        return self._fields
+	self.visit(tree)
+	return self._fields
 
-    def visit(self, tree):
-        label = str(tree)
+    def visit_Assign(self, tree):
+        print "Visiting assign"
+	rhs = tree.value
+	if len(tree.targets)==1:
+	    try:
+	        # subscript -> attribute -> name
+	        objname = rhs.value.value.id
+		# subscript -> attribute -> string
+		objmember = rhs.value.attr
+		fieldholders = ['scalar_fields', 'vector_fields', 'tensor_fields']
+		print objname, objmember
+		if objname == "state" and objmember in fieldholders:
+		    fieldname = rhs.slice.value.elts[0].s
+		    self._fields.append(fieldname)
+	    except AttributeError:
+	        # This is not a field access, so no action necessary.
+		pass
 
-        if label == '=':
-            rhs = tree.getChild(1)
-            if str(rhs) in ['scalar_fields', 'vector_fields', 'tensor_fields']:
-                field = str(rhs.getChild(0))
-                # Strip off the quotes
-                field = field[1:-1]
-                self._fields.append(field)
+	    
 
-    def pop(self):
-        pass
+#class AccessedFieldFinder(AntlrVisitor):
+#
+#    def __init__(self):
+#        AntlrVisitor.__init__(self, preOrder)
+#
+#    def find(self, tree):
+#        self._fields = []
+#        self.traverse(tree)
+#        return self._fields
+#
+#    def visit(self, tree):
+#        label = str(tree)
+#
+#        if label == '=':
+#            rhs = tree.getChild(1)
+#            if str(rhs) in ['scalar_fields', 'vector_fields', 'tensor_fields']:
+#                field = str(rhs.getChild(0))
+#                # Strip off the quotes
+#                field = field[1:-1]
+#                self._fields.append(field)
+#
+#    def pop(self):
+#        pass
 
 def findAccessedFields(tree):
     AFF = AccessedFieldFinder()
