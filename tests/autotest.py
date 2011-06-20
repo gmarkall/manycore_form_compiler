a81800bd0d9d653b72ccbd98c7319a5733ca6485
Florian Rathgeber <f.rathgeber10@imperial.ac.uk>
2011-06-20 11:25:56 +0100

    Refactor option file parser and add command line parameters
    The AutoTest class now has a test method, which is called with the test
    hook, input/output/expected, sources and an optional message. The
    AutoTester constructor takes a flag for (non-)interactive operation.
    
    CUDA and option file parser tests can be disabled with command line
    parameters. Single test files on the command line are supported.

--- tests/autotest.py
+++ tests/autotest.py
@@ -1,9 +1,15 @@
 """
-Autotest framework for mcfc.
+Autotest framework for MCFC.
 
-Runs the mcfc on each of the ufl files in inputs, stores the output in outputs,
+Runs the MCFc on each of the ufl files in inputs, stores the output in outputs,
 and compares them with those in expected. Anomalies are highlighted, and the
 user may replace the testcase with the new output. 
+
+usage: autotest.py [OPTIONS] [INPUT-FILE]
+    where OPTIONS can be one of
+        -n, --non-interactive  Run in non-interactive (batch) mode
+        --no-cuda              Do not test CUDA output
+        --no-optionfile        Do not test the option file parser
 """
 
 # Python modules
@@ -21,28 +27,41 @@ from pygments.formatters import TerminalFormatter
 # MCFC modules
 from mcfc import frontend, optionfileparser
 
-# Global status
-failed = 0
-interactive = True
-
 def main():
 
     sys.excepthook = ultraTB.FormattedTB(mode='Context')
 
     opts, args = get_options()
     keys = opts.keys()
-
-    # Check a single file if specified. Otherwise check
-    # all those specified above.
-    if (len(args) > 0):
-        sources = [ args[0] ]
  
     # Check for non-interactive execution (e.g. on the
-    # buildbot.
-    if 'noninteractive' in keys or 'n' in keys:
+    # buildbot).
+    if 'non-interactive' in keys or 'n' in keys:
         print "Running in non-interactive mode."
-        global interactive
-        interactive = False
+        tester = AutoTester(False)
+    else:
+        tester = AutoTester()
+
+    check_cuda =  'no-cuda' not in keys
+    check_optionfile = 'no-optionfile' not in keys
+
+    ufl_sources = ['diffusion-1', 'diffusion-2', 'diffusion-3', 'identity', \
+            'laplacian', 'helmholtz', 'euler-advection', 'identity-vector' ]
+    optionfile_sources = ['test', 'cdisk_adv_diff']
+
+    # Check a single file if specified. Otherwise check
+    # all default input files.
+    if (len(args) > 0):
+        if args[0] in ufl_sources:
+            ufl_sources = [ args[0] ]
+            check_optionfile = False
+        elif args[0] in optionfile_sources:
+            optionfile_sources = [ args[0] ]
+            check_cuda = False
+        else:
+            print "Unsupported source file:",args[0]
+            print "Needs to be in",ufl_sources,"or",optionfile_sources
+            sys.exit(-1)
 
     # Delete the outputs folder if it exists, to avoid
     # any stale files.
@@ -52,50 +71,36 @@ def main():
     # Create the outputs folder
     os.mkdir('outputs', 0755)
 
-    test_formcompiler()
-    test_optionfileparser()
+    if check_cuda:
+
+        # Create the cuda outputs folder
+        os.mkdir('outputs/cuda', 0755)
+
+        tester.test(frontend.testHook, 
+            lambda name: "inputs/ufl/" + name + ".ufl",
+            lambda name: "outputs/cuda/" + name + ".cu",
+            lambda name: "expected/cuda/" + name + ".cu",
+            ufl_sources,
+            'Running form compiler tests...')
+
+    if check_optionfile:
+
+        # Create the option file outputs folder
+        os.mkdir('outputs/optionfile', 0755)
+
+        tester.test(optionfileparser.testHook, 
+            lambda name: "inputs/flml/" + name + ".flml",
+            lambda name: "outputs/optionfile/" + name + ".dat",
+            lambda name: "expected/optionfile/" + name + ".dat",
+            optionfile_sources,
+            'Running option file parser tests...')
 
     # Exit code is 0 if no tests failed.
-    sys.exit(failed)
-
-def test_formcompiler():
-
-    print 'Running form compiler tests...'
-
-    sources = ['diffusion-1', 'diffusion-2', 'diffusion-3', 'identity', \
-               'laplacian', 'helmholtz', 'euler-advection', 'identity-vector' ]
-
-    tester = AutoTester(frontend.testHook, 
-        lambda name: "inputs/ufl/" + name + ".ufl",
-        lambda name: "outputs/cuda/" + name + ".cu",
-        lambda name: "expected/cuda/" + name + ".cu")
-
-    # Create the outputs folder
-    os.mkdir('outputs/cuda', 0755)
-
-    for sourcefile in sources:
-        tester.check(sourcefile)
-
-def test_optionfileparser():
-
-    print 'Running option file parser tests...'
-
-    sources = ['test', 'cdisk_adv_diff']
-
-    tester = AutoTester(optionfileparser.testHook, 
-        lambda name: "inputs/flml/" + name + ".flml",
-        lambda name: "outputs/optionfile/" + name + ".dat",
-        lambda name: "expected/optionfile/" + name + ".dat")
-
-    # Create the outputs folder
-    os.mkdir('outputs/optionfile', 0755)
-
-    for sourcefile in sources:
-        tester.check(sourcefile)
+    sys.exit(tester.failed)
 
 def get_options():
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "n", ["noninteractive"])
+        opts, args = getopt.getopt(sys.argv[1:], "n", ["non-interactive", "no-cuda", "no-optionfile"])
     except getopt.error, msg:
         print msg
         print __doc__
@@ -111,11 +116,21 @@ def get_options():
 
 class AutoTester:
 
-    def __init__(self, testhook, infile, outfile, expectfile):
+    def __init__(self, interactive = True):
+        self.interactive = interactive
+        self.failed = 0
+
+    def test(self, testhook, infile, outfile, expectfile, sources, message = None):
         self.testhook = testhook
         self.infile = infile
         self.outfile = outfile
         self.expectfile = expectfile
+
+        if message:
+            print message
+
+        for sourcefile in sources:
+            self.check(sourcefile)
 
     def check(self, sourcefile):
 
@@ -132,8 +147,7 @@ class AutoTester:
         diffout, differr = diff.communicate()
         if differr:
             print differr
-            global failed
-            failed = 1
+            self.failed = 1
         else:
             self.check_diff(sourcefile, diffout)
 
@@ -141,10 +155,9 @@ class AutoTester:
 
         if diff:
             print "    Difference detected in %s." % sourcefile
-            global failed
-            failed = 1
+            self.failed = 1
             
-            if interactive:
+            if self.interactive:
                 self.diffmenu(sourcefile, diff)
 
     def diffmenu(self, sourcefile, diffout):
