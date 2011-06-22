843799f83dd93c67d0ffa425bdabf00de5f5e73f
Florian Rathgeber <f.rathgeber10@imperial.ac.uk>
2011-06-22 19:12:30 +0100

    uniqify is now a generator, use uniqify_list if you want a list returned.

--- mcfc/utilities.py
+++ mcfc/utilities.py
@@ -1,5 +1,5 @@
 # From http://www.peterbe.com/plog/uniqifiers-benchmark. This is version f5.
-def uniqify(seq, idfun=None):
+def uniqify_list(seq, idfun=None):
     if idfun is None:
         def idfun(x): return x
     seen = {}
@@ -10,5 +10,15 @@ def uniqify(seq, idfun=None):
         seen[marker] = 1
         result.append(item)
     return result
+
+def uniqify(seq, idfun=None):
+    if idfun is None:
+        def idfun(x): return x
+    seen = {}
+    for item in seq:
+        marker = idfun(item)
+        if marker in seen: continue
+        seen[marker] = 1
+        yield item
 
 # vim:sw=4:ts=4:sts=4:et
