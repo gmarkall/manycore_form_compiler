db331f8d20808d674262f085a1639cbbce5ce5d1
Florian Rathgeber <f.rathgeber10@imperial.ac.uk>
2011-06-23 12:37:36 +0100

    Add utility method returning an embedded IPython shell

--- mcfc/utilities.py
+++ mcfc/utilities.py
@@ -21,4 +21,10 @@ def uniqify(seq, idfun=None):
         seen[marker] = 1
         yield item
 
+def embedded_ipython_shell(msg=None):
+    "Return an embedded IPython shell. Call to drop into the shell."
+    from IPython.Shell import IPShellEmbed
+    ipshell = IPShellEmbed(banner=msg or "Dropping into IPython shell...")
+    return ipshell
+
 # vim:sw=4:ts=4:sts=4:et
