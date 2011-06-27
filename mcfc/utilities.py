395821972feea842a148372e1f8fbecf2d22dcf9
Florian Rathgeber <f.rathgeber10@imperial.ac.uk>
2011-06-27 13:59:40 +0100

    Add non order-preserving and recursive uniqify functions

--- mcfc/utilities.py
+++ mcfc/utilities.py
@@ -1,7 +1,6 @@
 # From http://www.peterbe.com/plog/uniqifiers-benchmark. This is version f5.
 def uniqify_list(seq, idfun=None):
-    if idfun is None:
-        def idfun(x): return x
+    idfun = idfun or (lambda x: x)
     seen = {}
     result = []
     for item in seq:
@@ -11,16 +10,36 @@ def uniqify_list(seq, idfun=None):
         result.append(item)
     return result
 
-def uniqify(seq, idfun=None):
-    if idfun is None:
-        def idfun(x): return x
+# From http://code.activestate.com/recipes/52560-remove-duplicates-from-a-sequence/
+def uniqify_unordered(seq):
     seen = {}
+    for item in seq:
+        seen[item] = 1
+    return seen.keys()
+
+def uniqify(seq, idfun=None, seen=None):
+    idfun = idfun or (lambda x: x)
+    seen = seen or {}
     for item in seq:
         marker = idfun(item)
         if marker in seen: continue
         seen[marker] = 1
         yield item
 
+def uniqify_rec(seq, idfun=None, seen=None):
+    idfun = idfun or (lambda x: x)
+    seen = seen or {}
+    for item in seq:
+        try:
+            for s in uniqify_rec(item, idfun, seen):
+                yield s
+        except TypeError:
+            marker = idfun(item)
+            if marker in seen: continue
+            seen[marker] = 1
+            yield item
+
+# From http://ipython.scipy.org/doc/manual/html/interactive/reference.html#embedding-ipython
 def embedded_ipython_shell(msg=None):
     "Return an embedded IPython shell. Call to drop into the shell."
     from IPython.Shell import IPShellEmbed
