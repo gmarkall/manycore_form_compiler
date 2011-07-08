c607d3f632d0724bbc390097deed1ba23c98977b
gmarkall <grm08@doc.ic.ac.uk>
2011-07-08 20:07:58 +0100

    Forgot to update autotests for the number of threads.

--- tests/expected/cuda/diffusion-3.cu
+++ tests/expected/cuda/diffusion-3.cu
@@ -156,7 +156,7 @@ extern "C" void run_model_(double* dt_pointer)
   int nodesPerEle = (state -> getNodesPerEle("Coordinate"));
   double* shape = (state -> getBasisFunction("Coordinate"));
   double* dShape = (state -> getBasisFunctionDerivative("Coordinate"));
-  int blockXDim = 1;
+  int blockXDim = 2;
   int gridXDim = 30;
   int shMemSize = t2p_shmemsize(blockXDim, nDim, nodesPerEle);
   transform_to_physical<<<gridXDim,blockXDim,shMemSize>>>(coordinates, dn, quadWeights, dShape, detwei, numEle, nDim, nQuad, nodesPerEle);
