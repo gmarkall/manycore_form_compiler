18581c6b6e49fdb1d65be70b92c833d899122f66
gmarkall <grm08@doc.ic.ac.uk>
2011-12-13 13:09:48 +0000

    Remove #ifdefs that were used to prevent the ROSE-based form compiler from tripping up.

--- fluidity/cudastatic.hpp
+++ fluidity/cudastatic.hpp
@@ -8,33 +8,18 @@
 
 // Hide the __global__ keywords from the ROSE compiler.
 
-#ifdef __CUDACC__
-__global__
-#endif
-void transform_to_physical(double *node_coords, double *dn, double *quad_weights, double *dshape, double *detwei, int n_ele, int n_dim, int n_quad, int nodes_per_ele);
+__global__ void transform_to_physical(double *node_coords, double *dn, double *quad_weights, double *dshape, double *detwei, int n_ele, int n_dim, int n_quad, int nodes_per_ele);
 
-#ifdef __CUDACC__
-__global__
-#endif
-void matrix_addto(int* glob_mat_findrm, int* glob_mat_colm, double* glob_mat_val, int *local_to_global, double *local_matrices, int n_ele, int nodes_per_ele);
+__global__ void matrix_addto(int* glob_mat_findrm, int* glob_mat_colm, double* glob_mat_val, int *local_to_global, double *local_matrices, int n_ele, int nodes_per_ele);
 
-#ifdef __CUDACC__
-__global__
-#endif
-void vector_addto(double* glob_vec, int *local_to_global, double *local_vectors, int n_ele, int nodes_per_ele);
+__global__ void vector_addto(double* glob_vec, int *local_to_global, double *local_vectors, int n_ele, int nodes_per_ele);
 
 void cg_solve(int* findrm, int size_findrm, int* colm, int size_colm, double* k_val, double* b, int rhs_size, double *x);
 
 // Change data from the one-value-per node to one-value-per-node-per element layout and back
-#ifdef __CUDACC__
-__global__
-#endif
-void expand_data(double* dest, double* src, int* node_nums, int n_ele, int n_vals_per_node, int nodes_per_ele);
+__global__ void expand_data(double* dest, double* src, int* node_nums, int n_ele, int n_vals_per_node, int nodes_per_ele);
 
-#ifdef __CUDACC__
-__global__
-#endif
-void contract_data(double* dest, double* src, int* node_nums, int n_ele, int n_vals_per_node, int nodes_per_ele);
+__global__ void contract_data(double* dest, double* src, int* node_nums, int n_ele, int n_vals_per_node, int nodes_per_ele);
 
 int t2p_shmemsize(int block_x_dim, int n_dim, int nodes_per_ele);
 
