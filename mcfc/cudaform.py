3cbf489818d47fa695dc5c3f8afa7afca7d816c3
gmarkall <grm08@doc.ic.ac.uk>
2012-01-10 11:11:20 +0000

    Remove call to self.buildGaussIndex in op2form and cudaform

--- mcfc/cudaform.py
+++ mcfc/cudaform.py
@@ -55,7 +55,7 @@ class CudaFormBackend(FormBackend):
         return KPG.generate(tree, form, statutoryParameters)
 
     def subscript_detwei(self):
-        indices = [ElementIndex(), self.buildGaussIndex()]
+        indices = [ElementIndex(), buildGaussIndex(self.numGaussPoints)]
         return indices
 
 # vim:sw=4:ts=4:sts=4:et
