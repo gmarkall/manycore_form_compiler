gmarkall/manycore_form_compiler

commit e9e46ccd8af12d10ccda2c12dc6b0f79fc4c2dc8

Remove self._formBackend.buildBasisIndex from op2expression.py

mcfc/op2expression.py
@@ -43,7 +43,8 @@ class Op2ExpressionBuilder(ExpressionBuilder):
     def subscript_Argument(self, tree):
         # Build the subscript based on the argument count
         count = tree.count()
-        indices = [self._formBackend.buildBasisIndex(count),
+        element = tree.element()
+        indices = [buildBasisIndex(count, element),
                    self._formBackend.buildGaussIndex()]
         return indices
 
@@ -52,12 +53,13 @@ class Op2ExpressionBuilder(ExpressionBuilder):
         # nesting depth of IndexSums of the expression.
         operand, _ = tree.operands()
         count = operand.count()
+        element = operand.element()
 
         if isinstance(operand, Argument):
             indices = []
             indices.extend(dimIndices)
             indices = indices + [ self._formBackend.buildGaussIndex(),
-                                  self._formBackend.buildBasisIndex(count) ]
+                                  buildBasisIndex(count, element) ]
         elif isinstance(operand, Coefficient):
             indices = [ self._formBackend.buildGaussIndex() ]
             indices.extend(dimIndices)
@@ -71,7 +73,7 @@ class Op2ExpressionBuilder(ExpressionBuilder):
         indices = []
         # One rank index for each rank
         for r in range(rank):
-            indices.append(self._formBackend.buildBasisIndex(r))
+            indices.append(buildBasisIndex(r, form))
 
         return indices
 
@@ -81,20 +83,27 @@ class Op2QuadratureExpressionBuilder(QuadratureExpressionBuilder):
         return buildSubscript(variable, indices)
 
     def subscript(self, tree):
+        if isinstance(tree, Coefficient):
+            element = tree.element()
+        elif isinstance(tree, SpatialDerivative):
+            element = tree.operands()[0].element()
+        
+        dim = element.cell().topological_dimension()
         rank = tree.rank()
         # Subscript order: basis index followed by dimension indices (if any)
-        indices = [self._formBackend.buildBasisIndex(0)]
+        indices = [buildBasisIndex(0, element)]
         for r in range(rank):
             indices.append(self._formBackend.buildDimIndex(r))
         return indices
 
     def subscript_spatial_derivative(self, tree):
+        element = tree.operands()[0].element()
         # The count of the basis function induction variable is always
         # 0 in the quadrature loops (i.e. i_r_0), and only the first dim
         # index should be used to subscript the derivative (I think).
         indices = [ self._formBackend.buildDimIndex(0),
                     self._formBackend.buildGaussIndex(),
-                    self._formBackend.buildBasisIndex(0) ]
+                    buildBasisIndex(0, element) ]
         return indices
 
 # vim:sw=4:ts=4:sts=4:et

tests/expected/op2/diffusion-1.cpp
@@ -4,7 +4,7 @@
 
 
 
-void A(double localTensor[3][3], double dt, double detwei[6], double c0[3][2][2], double CG1[3][6], double d_CG1[2][6][3])
+void A(double localTensor[3][3], double dt, double detwei[6], double c0[12][2][2], double CG1[3][6], double d_CG1[2][6][3])
 {
   double c_q0[6][2][2];
   for(int i_g = 0; i_g < 6; i_g++)
@@ -41,7 +41,7 @@ void A(double localTensor[3][3], double dt, double detwei[6], double c0[3][2][2]
   };
 }
 
-void d(double localTensor[3][3], double dt, double detwei[6], double c0[3][2][2], double d_CG1[2][6][3])
+void d(double localTensor[3][3], double dt, double detwei[6], double c0[12][2][2], double d_CG1[2][6][3])
 {
   double c_q0[6][2][2];
   for(int i_g = 0; i_g < 6; i_g++)
@@ -92,7 +92,7 @@ void M(double localTensor[3][3], double dt, double detwei[6], double CG1[3][6])
   };
 }
 
-void rhs(double localTensor[3], double dt, double detwei[6], double c0[3], double c1[3][2][2], double CG1[3][6], double d_CG1[2][6][3])
+void rhs(double localTensor[3], double dt, double detwei[6], double c0[3], double c1[12][2][2], double CG1[3][6], double d_CG1[2][6][3])
 {
   double c_q0[6];
   double c_q1[6][2][2];

tests/expected/op2/diffusion-2.cpp
@@ -4,7 +4,7 @@
 
 
 
-void A(double localTensor[3][3], double dt, double detwei[6], double c0[3][2][2], double CG1[3][6], double d_CG1[2][6][3])
+void A(double localTensor[3][3], double dt, double detwei[6], double c0[12][2][2], double CG1[3][6], double d_CG1[2][6][3])
 {
   double c_q0[6][2][2];
   for(int i_g = 0; i_g < 6; i_g++)
@@ -41,7 +41,7 @@ void A(double localTensor[3][3], double dt, double detwei[6], double c0[3][2][2]
   };
 }
 
-void d(double localTensor[3][3], double dt, double detwei[6], double c0[3][2][2], double d_CG1[2][6][3])
+void d(double localTensor[3][3], double dt, double detwei[6], double c0[12][2][2], double d_CG1[2][6][3])
 {
   double c_q0[6][2][2];
   for(int i_g = 0; i_g < 6; i_g++)
@@ -92,7 +92,7 @@ void M(double localTensor[3][3], double dt, double detwei[6], double CG1[3][6])
   };
 }
 
-void rhs(double localTensor[3], double dt, double detwei[6], double c0[3], double c1[3][2][2], double CG1[3][6], double d_CG1[2][6][3])
+void rhs(double localTensor[3], double dt, double detwei[6], double c0[3], double c1[12][2][2], double CG1[3][6], double d_CG1[2][6][3])
 {
   double c_q0[6];
   double c_q1[6][2][2];

tests/expected/op2/euler-advection.cpp
@@ -19,7 +19,7 @@ void Mass(double localTensor[3][3], double dt, double detwei[6], double CG1[3][6
   };
 }
 
-void rhs(double localTensor[3], double dt, double detwei[6], double c0[3], double c1[3][2], double CG1[3][6], double d_CG1[2][6][3])
+void rhs(double localTensor[3], double dt, double detwei[6], double c0[3], double c1[6][2], double CG1[3][6], double d_CG1[2][6][3])
 {
   double c_q0[6];
   double c_q1[6][2];

tests/expected/op2/identity-vector.cpp
@@ -4,7 +4,7 @@
 
 
 
-void A(double localTensor[3][3], double dt, double detwei[6], double CG1[3][6])
+void A(double localTensor[6][6], double dt, double detwei[6], double CG1[3][6])
 {
   double CG1_v[2][6][6] = { { { CG1[0], CG1[6], CG1[12], 0.0, 0.0, 0.0 }, { CG1[1], CG1[7], CG1[13], 0.0, 0.0, 0.0 }, { CG1[2], CG1[8], CG1[14], 0.0, 0.0, 0.0 }, { CG1[3], CG1[9], CG1[15], 0.0, 0.0, 0.0 }, { CG1[4], CG1[10], CG1[16], 0.0, 0.0, 0.0 }, { CG1[5], CG1[11], CG1[17], 0.0, 0.0, 0.0 } }, { { 0.0, 0.0, 0.0, CG1[0], CG1[6], CG1[12] }, { 0.0, 0.0, 0.0, CG1[1], CG1[7], CG1[13] }, { 0.0, 0.0, 0.0, CG1[2], CG1[8], CG1[14] }, { 0.0, 0.0, 0.0, CG1[3], CG1[9], CG1[15] }, { 0.0, 0.0, 0.0, CG1[4], CG1[10], CG1[16] }, { 0.0, 0.0, 0.0, CG1[5], CG1[11], CG1[17] } } };
   for(int i_r_0 = 0; i_r_0 < 6; i_r_0++)
@@ -23,7 +23,7 @@ void A(double localTensor[3][3], double dt, double detwei[6], double CG1[3][6])
   };
 }
 
-void RHS(double localTensor[3], double dt, double detwei[6], double c0[3][2], double CG1[3][6])
+void RHS(double localTensor[6], double dt, double detwei[6], double c0[6][2], double CG1[3][6])
 {
   double CG1_v[2][6][6] = { { { CG1[0], CG1[6], CG1[12], 0.0, 0.0, 0.0 }, { CG1[1], CG1[7], CG1[13], 0.0, 0.0, 0.0 }, { CG1[2], CG1[8], CG1[14], 0.0, 0.0, 0.0 }, { CG1[3], CG1[9], CG1[15], 0.0, 0.0, 0.0 }, { CG1[4], CG1[10], CG1[16], 0.0, 0.0, 0.0 }, { CG1[5], CG1[11], CG1[17], 0.0, 0.0, 0.0 } }, { { 0.0, 0.0, 0.0, CG1[0], CG1[6], CG1[12] }, { 0.0, 0.0, 0.0, CG1[1], CG1[7], CG1[13] }, { 0.0, 0.0, 0.0, CG1[2], CG1[8], CG1[14] }, { 0.0, 0.0, 0.0, CG1[3], CG1[9], CG1[15] }, { 0.0, 0.0, 0.0, CG1[4], CG1[10], CG1[16] }, { 0.0, 0.0, 0.0, CG1[5], CG1[11], CG1[17] } } };
   double c_q0[6][2];

tests/expected/op2/simple-advection-diffusion.cpp
@@ -90,7 +90,7 @@ void diff_rhs(double localTensor[3], double dt, double detwei[6], double c0[3], 
   };
 }
 
-void adv_rhs(double localTensor[3], double dt, double detwei[6], double c0[3], double c1[3][2], double CG1[3][6], double d_CG1[2][6][3])
+void adv_rhs(double localTensor[3], double dt, double detwei[6], double c0[3], double c1[6][2], double CG1[3][6], double d_CG1[2][6][3])
 {
   double c_q0[6];
   double c_q1[6][2];

commit dabfaa242a19f57cd541ab4359024720c562d97b

Fix basis index extent for coefficients (regression from e9e46ccd).
We index vector/tensor valued coefficients by separate indices for the
scalar basis and the spatial dimension(s). Hence we need to extract
the scalar element to build the appropriate basis index for
subscripting a coefficient in the QuadratureExpressionBuilder.

Note that the way coefficients received as parameters are indexed is
defined by the API MCFC generates code for.

The OP2 specification states that a vector-valued coefficient be
indexed by separate indices for the scalar basis and the spatial
dimension(s) (same for tensor-valued coefficients).

We use the scalar basis for the expression evaluating a coefficient
at the quadrature points even if the coefficient is on a vector or
tensor basis since that is (in UFL) by definition a tensor product of
the scalar basis. So we need to extract the scalar element in this
case as well.

mcfc/cudaexpression.py
@@ -136,10 +136,13 @@ class CudaQuadratureExpressionBuilder(QuadratureExpressionBuilder):
         return buildSubscript(variable, indices)
 
     def subscript(self, tree):
+        # We index vector/tensor valued coefficients by separate indices for
+        # the scalar basis and the spatial dimension(s). Hence we need to
+        # extract the scalar element to build the appropriate basis index.
         indices = [ ElementIndex() ]
         for r in range(tree.rank()):
             indices.append(buildDimIndex(r,tree))
-        indices.append(buildBasisIndex(0, extract_element(tree)))
+        indices.append(buildBasisIndex(0, extract_subelement(tree)))
         return indices
 
     def subscript_spatial_derivative(self, tree):
@@ -149,7 +152,7 @@ class CudaQuadratureExpressionBuilder(QuadratureExpressionBuilder):
         indices = [ ElementIndex(),
                     buildDimIndex(0, tree),
                     buildGaussIndex(self._formBackend.numGaussPoints),
-                    buildBasisIndex(0, extract_element(tree)) ]
+                    buildBasisIndex(0, extract_subelement(tree)) ]
         return indices
 
 # vim:sw=4:ts=4:sts=4:et

mcfc/expression.py
@@ -231,15 +231,13 @@ class QuadratureExpressionBuilder:
         raise NotImplementedError("You're supposed to implement subscript()!")
 
     def subscript_argument(self, tree):
-        # FIXME: At present we make use of the scalar basis for the expression
-        # even if the coefficient is on a vector or tensor basis. So we need to
-        # extract a single element to use as the basis.
-        e = tree.element()
-        if isinstance(e, (VectorElement, TensorElement)):
-            e = e.sub_elements()[0]
         # The count of the basis function induction variable is always
         # 0 in the quadrature loops (i.e. i_r_0)
-        indices = [buildBasisIndex(0, e),
+        # We use the scalar basis for the expression evaluating a coefficient
+        # at the quadrature points even if the coefficient is on a vector or
+        # tensor basis since that is (in UFL) by definition a tensor product of
+        # the scalar basis. So we need to extract the sub element.
+        indices = [buildBasisIndex(0, extract_subelement(tree)),
                    buildGaussIndex(self._formBackend.numGaussPoints)]
         return indices
 

mcfc/op2expression.py
@@ -84,8 +84,11 @@ class Op2QuadratureExpressionBuilder(QuadratureExpressionBuilder):
         return buildSubscript(variable, indices)
 
     def subscript(self, tree):
-        # Subscript order: basis index followed by dimension indices (if any)
-        indices = [buildBasisIndex(0, extract_element(tree))]
+        # The OP2 specification states that a vector-valued coefficient be
+        # indexed by separate indices for the scalar basis and the spatial
+        # dimension(s) (same for tensor-valued coefficients). Hence we need to
+        # extract the scalar element to build the appropriate basis index.
+        indices = [buildBasisIndex(0, extract_subelement(tree))]
         for r in range(tree.rank()):
             indices.append(buildDimIndex(r,tree))
         return indices
@@ -96,7 +99,7 @@ class Op2QuadratureExpressionBuilder(QuadratureExpressionBuilder):
         # index should be used to subscript the derivative (I think).
         indices = [ buildDimIndex(0,tree),
                     buildGaussIndex(self._formBackend.numGaussPoints),
-                    buildBasisIndex(0, extract_element(tree)) ]
+                    buildBasisIndex(0, extract_subelement(tree)) ]
         return indices
 
 # vim:sw=4:ts=4:sts=4:et

tests/expected/op2/diffusion-1.cpp
@@ -4,7 +4,7 @@
 
 
 
-void A(double localTensor[3][3], double dt, double detwei[6], double c0[12][2][2], double CG1[3][6], double d_CG1[2][6][3])
+void A(double localTensor[3][3], double dt, double detwei[6], double c0[3][2][2], double CG1[3][6], double d_CG1[2][6][3])
 {
   double c_q0[6][2][2];
   for(int i_g = 0; i_g < 6; i_g++)
@@ -41,7 +41,7 @@ void A(double localTensor[3][3], double dt, double detwei[6], double c0[12][2][2
   };
 }
 
-void d(double localTensor[3][3], double dt, double detwei[6], double c0[12][2][2], double d_CG1[2][6][3])
+void d(double localTensor[3][3], double dt, double detwei[6], double c0[3][2][2], double d_CG1[2][6][3])
 {
   double c_q0[6][2][2];
   for(int i_g = 0; i_g < 6; i_g++)
@@ -92,7 +92,7 @@ void M(double localTensor[3][3], double dt, double detwei[6], double CG1[3][6])
   };
 }
 
-void rhs(double localTensor[3], double dt, double detwei[6], double c0[3], double c1[12][2][2], double CG1[3][6], double d_CG1[2][6][3])
+void rhs(double localTensor[3], double dt, double detwei[6], double c0[3], double c1[3][2][2], double CG1[3][6], double d_CG1[2][6][3])
 {
   double c_q0[6];
   double c_q1[6][2][2];

tests/expected/op2/diffusion-2.cpp
@@ -4,7 +4,7 @@
 
 
 
-void A(double localTensor[3][3], double dt, double detwei[6], double c0[12][2][2], double CG1[3][6], double d_CG1[2][6][3])
+void A(double localTensor[3][3], double dt, double detwei[6], double c0[3][2][2], double CG1[3][6], double d_CG1[2][6][3])
 {
   double c_q0[6][2][2];
   for(int i_g = 0; i_g < 6; i_g++)
@@ -41,7 +41,7 @@ void A(double localTensor[3][3], double dt, double detwei[6], double c0[12][2][2
   };
 }
 
-void d(double localTensor[3][3], double dt, double detwei[6], double c0[12][2][2], double d_CG1[2][6][3])
+void d(double localTensor[3][3], double dt, double detwei[6], double c0[3][2][2], double d_CG1[2][6][3])
 {
   double c_q0[6][2][2];
   for(int i_g = 0; i_g < 6; i_g++)
@@ -92,7 +92,7 @@ void M(double localTensor[3][3], double dt, double detwei[6], double CG1[3][6])
   };
 }
 
-void rhs(double localTensor[3], double dt, double detwei[6], double c0[3], double c1[12][2][2], double CG1[3][6], double d_CG1[2][6][3])
+void rhs(double localTensor[3], double dt, double detwei[6], double c0[3], double c1[3][2][2], double CG1[3][6], double d_CG1[2][6][3])
 {
   double c_q0[6];
   double c_q1[6][2][2];

tests/expected/op2/euler-advection.cpp
@@ -19,7 +19,7 @@ void Mass(double localTensor[3][3], double dt, double detwei[6], double CG1[3][6
   };
 }
 
-void rhs(double localTensor[3], double dt, double detwei[6], double c0[3], double c1[6][2], double CG1[3][6], double d_CG1[2][6][3])
+void rhs(double localTensor[3], double dt, double detwei[6], double c0[3], double c1[3][2], double CG1[3][6], double d_CG1[2][6][3])
 {
   double c_q0[6];
   double c_q1[6][2];

tests/expected/op2/identity-vector.cpp
@@ -23,7 +23,7 @@ void A(double localTensor[6][6], double dt, double detwei[6], double CG1[3][6])
   };
 }
 
-void RHS(double localTensor[6], double dt, double detwei[6], double c0[6][2], double CG1[3][6])
+void RHS(double localTensor[6], double dt, double detwei[6], double c0[3][2], double CG1[3][6])
 {
   double CG1_v[2][6][6] = { { { CG1[0], CG1[6], CG1[12], 0.0, 0.0, 0.0 }, { CG1[1], CG1[7], CG1[13], 0.0, 0.0, 0.0 }, { CG1[2], CG1[8], CG1[14], 0.0, 0.0, 0.0 }, { CG1[3], CG1[9], CG1[15], 0.0, 0.0, 0.0 }, { CG1[4], CG1[10], CG1[16], 0.0, 0.0, 0.0 }, { CG1[5], CG1[11], CG1[17], 0.0, 0.0, 0.0 } }, { { 0.0, 0.0, 0.0, CG1[0], CG1[6], CG1[12] }, { 0.0, 0.0, 0.0, CG1[1], CG1[7], CG1[13] }, { 0.0, 0.0, 0.0, CG1[2], CG1[8], CG1[14] }, { 0.0, 0.0, 0.0, CG1[3], CG1[9], CG1[15] }, { 0.0, 0.0, 0.0, CG1[4], CG1[10], CG1[16] }, { 0.0, 0.0, 0.0, CG1[5], CG1[11], CG1[17] } } };
   double c_q0[6][2];

tests/expected/op2/simple-advection-diffusion.cpp
@@ -90,7 +90,7 @@ void diff_rhs(double localTensor[3], double dt, double detwei[6], double c0[3], 
   };
 }
 
-void adv_rhs(double localTensor[3], double dt, double detwei[6], double c0[3], double c1[6][2], double CG1[3][6], double d_CG1[2][6][3])
+void adv_rhs(double localTensor[3], double dt, double detwei[6], double c0[3], double c1[3][2], double CG1[3][6], double d_CG1[2][6][3])
 {
   double c_q0[6];
   double c_q1[6][2];