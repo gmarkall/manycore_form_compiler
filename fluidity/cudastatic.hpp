bad5e107245a528cbdc49a84726f90f66e5cf7f0
gmarkall <grm08@doc.ic.ac.uk>
2012-05-11 09:04:40 +0100

    Need prototype for cg_sole_lma too.

--- fluidity/cudastatic.hpp
+++ fluidity/cudastatic.hpp
@@ -15,6 +15,7 @@ __global__ void matrix_addto(int* glob_mat_findrm, int* glob_mat_colm, double* g
 __global__ void vector_addto(double* glob_vec, int *local_to_global, double *local_vectors, int n_ele, int nodes_per_ele);
 
 void cg_solve(int* findrm, int size_findrm, int* colm, int size_colm, double* k_val, double* b, int rhs_size, double *x);
+void cg_solve_lma(int* k_findrm, int size_findrm, int* k_colm, int size_colm, double* k_val, double* k_b, int rhs_val_size, double *x_p, int n_ele, double *matrix, int *node_nums);
 
 // Change data from the one-value-per node to one-value-per-node-per element layout and back
 __global__ void expand_data(double* dest, double* src, int* node_nums, int n_ele, int n_vals_per_node, int nodes_per_ele);
